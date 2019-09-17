421f3b051bb149be2c25225c0f73f5c726962305
LimiO <52657506+LimiO@users.noreply.github.com>
2019-09-17 23:34:22 +0300

    Add files via upload

---
+++ config/config.py
@@ -0,0 +1,12 @@
+TOKEN = ''
+
+def autosending_text(bot, message):
+	first_name = bot.get_chat(message.chat.id).first_name
+	text = """Hello, {0}
+This is the starter template for other bots built with <b>python</b>. 
+
+No bots cooked so far.
+
+
+Enjoy cooking with <a href = 'https://github.com/LimiO/TutorialHeroku'>this template</a>!""".format(first_name)
+	return text
